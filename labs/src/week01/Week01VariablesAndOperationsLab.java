8b524c47d07edec4d0e79eb867710a4b0dfa63ec
Vince Lanzilotti <vlanzilotti@launchphilly.org>
2023-03-18 22:43:09 -0400

    Added Week2 lab assignment

---
+++ labs/src/week01/Week01VariablesAndOperationsLab.java
@@ -0,0 +1,77 @@
+//
+// Copyright (c) 2023 Promineo Tech
+// Author:  Promineo Tech Academic Team
+// Subject:  Variables & Operations Lab
+// Java Week 01 Lab  
+//
+package week01;
+
+public class Week01VariablesAndOperationsLab {
+
+	public static void main(String[] args) {
+		System.out.println("Week 1 Lab");
+
+		// 1. Create a variable to hold the quantity of 
+		//		available plane seats left on a flight		
+
+		// int AvailableSeats
+		
+		// 2. Create a variable to hold the cost of groceries at checkout
+
+		// float CostGroceries
+		
+		// 3. Create a variable to hold a person's middle initial
+		
+		// char MiddleInitial
+	
+		// 4. Create a variable to hold true if it's hot outside 
+		//		and false if it's cold outside
+		
+		// boolean IsItHot = True
+		
+		// 5. Create a variable to hold a customer's first name
+		
+		// String FirstName
+		
+		// 6. Create a variable to hold a street address
+
+		// String StreetAddress
+
+		// 7. Print all variables to the console
+		
+		// System.out.println(AvailableSeats, CostGroceries, MiddleInitial, IsItHot, FirstName, StreetAddress)
+
+		// 8. A customer booked 2 plane seats, 
+		//		remove 2 seats from the available seats variable
+		
+		// AvailableSeats = AvailableSeats - 2
+
+		// 9. Impulse candy bar purchase, add 2.15 to the grocery total
+		// 		costOfGroceries = costOfGroceries + 2.15;
+		
+		// CostGroceries = CostGroceries + 2.15
+
+		// 10.  The birth certificate was printed incorrectly, 
+		//		change the middle initial to something else
+		
+		// MiddleInitial = "P"
+		
+		// 11.  The season has changed, update the hot outside 
+		//			variable to be opposite of what it was
+
+		// IsItHot = False
+		
+		// 12.  Create a new variable called full name using the customer's first name, 
+		//			the middle initial, and a last name of your choice
+	
+		// String FullName = FirstName + " " + MiddleInitial + " " + "Johnson"
+
+		// 13.  Print a line to the console that introduces the customer and says they live
+		// 			at the address variable
+		
+		// System.out.println("Hello, my name is " + FullName + " and I live at " + StreetAddress)
+
+		
+		
+	}
+}
