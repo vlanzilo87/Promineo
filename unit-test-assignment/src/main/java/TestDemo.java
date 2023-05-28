b7aa3a7334808edffcb35e2a9d77356d1c13fd2b
Vince Lanzilotti <vlanzilotti@launchphilly.org>
2023-05-27 22:17:48 -0400

    added Test Demo code

---
+++ unit-test-assignment/src/main/java/TestDemo.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class TestDemo {
+	
+	public int addPositive(int a, int b) {
+		if((a > 0) && (b > 0)) {
+			return (a + b);
+	    } else {
+	    	throw new IllegalArgumentException("Both parameters must be positive!");
+	    }
+	}
+	
+	public int randomNumberSquared() {
+		int number = getRandomInt();
+		
+		return (number * number);
+	}
+	
+	int getRandomInt() {
+		Random random = new Random();
+		
+		return random.nextInt(10) + 1;
+	}
+}
---
+++ unit-test-assignment/src/test/java/TestDemoTest.java
@@ -0,0 +1,54 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class TestDemoTest {
+
+	private TestDemo testDemo;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	@ParameterizedTest
+	@MethodSource("TestDemoTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, boolean expectException) {	
+		if(!expectException) {
+			assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class);
+		}
+	}
+	
+	static Stream<Arguments> argumentsForAddPositive() {
+		return Stream.of(
+				arguments(2, 4, 6, false),
+				arguments(-2, 5, 3, true),
+				arguments(7, 0, 7, true),
+				arguments(10, 2, 12, false),
+				arguments(-3, 0, -3, true));
+	}
+	
+	@Test
+	void assertThatNumberSquaredIsCorrect() {
+		TestDemo mockDemo = spy(testDemo);
+		
+		doReturn(5).when(mockDemo).getRandomInt();
+		
+		int fiveSquared = mockDemo.randomNumberSquared();
+		
+		assertThat(fiveSquared).isEqualTo(25);
+	}
+}
